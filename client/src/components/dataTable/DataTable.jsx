d725f1cef1ecdd27b6762d7671c14a2d202cb3c5
Edward Vera <edwardismael1@gmail.com>
2023-11-26 12:59:36 -0300

    Pagination functionality added

--- client/src/components/dataTable/DataTable.jsx
+++ client/src/components/dataTable/DataTable.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header.jsx'
 import Sections from './Sections'
 import { useSelector } from "react-redux";
 import styles from "./DataTable.module.css"
 import Product from './Product';
-import Pagination from './Pagination/Pagination.jsx';
+import Pagination from './Pagination.jsx';
 
 export default function DataTable() {
   const allProducts = useSelector((state) => state.allProducts);
   //const productsCopy = useSelector((state) => state.productsCopy);
-  console.log(allProducts);
+  //console.log(allProducts);
+  const itemsPerPage = 7;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(allProducts.length / itemsPerPage);
+
+  //Funcion para setear la pagina actual
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  //Creo un arreglo con el rango de los productos a mostrar
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const productsToDisplay = allProducts.slice(startIndex, endIndex);
+
   return (
     <div className=" bg-gray-50 shadow-md rounded-lg p-4 overflow-hidden overflow-x-auto text-center w-[80%]">
         <Header/>
@@ -21,10 +35,10 @@ export default function DataTable() {
                 <h4>Loading...</h4>
               </div>
             :
-            allProducts?.map((product, index) => (<Product product= {product} key={index}/>))
+            productsToDisplay.map((product, index) => <Product product={product} key={index} />)
           }
         </section>
-        <Pagination/>
+        <Pagination currentPage={currentPage} totalPages={totalPages} onChangePage={handlePageChange}/>
     </div>
   )
 }
---
+++ client/src/components/dataTable/Pagination.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function Pagination({ currentPage, totalPages, onChangePage }) {
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
+  return (
+    <div className=" w-full h-[60px] flex items-center justify-center gap-8 ">
+      <button
+        className="mx-1 px-3 py-2 rounded bg-gray-400"
+        onClick={() => onChangePage(1)}
+        disabled={isFirstPage}
+      >
+        1
+      </button>
+      <button
+        className="mx-1 px-3 py-2 rounded bg-gray-300"
+        onClick={() => onChangePage(currentPage - 1)}
+        disabled={isFirstPage}
+      >
+        &lt;
+      </button>
+      <button
+        className={`mx-1 px-3 py-2 rounded ${
+          currentPage === currentPage ? "bg-blue-500 text-white" : "bg-gray-300"
+        }`}
+        onClick={() => onChangePage(currentPage)}
+      >
+        {currentPage}
+      </button>
+      <button
+        className="mx-1 px-3 py-2 rounded bg-gray-300"
+        onClick={() => onChangePage(currentPage + 1)}
+        disabled={isLastPage}
+      >
+        &gt;
+      </button>
+      <button
+        className="mx-1 px-3 py-2 rounded bg-gray-400"
+        onClick={() => onChangePage(totalPages)}
+        disabled={isLastPage}
+      >
+        {totalPages}
+      </button>
+    </div>
+  );
+}
