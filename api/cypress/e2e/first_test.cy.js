528115d4d258fc04e94aff42522ba711b3b6d5d7
Edward Vera <edwardismael1@gmail.com>
2024-01-04 19:14:07 -0300

    fix production branch

--- api/cypress/e2e/first_test.cy.js
+++ api/cypress/e2e/first_test.cy.js
@@ -3,3 +3,23 @@ describe('Web Page', () => {
     cy.visit('https://prueba-edward-vera.vercel.app/')
   })
 })
+
+// describe("Test grupo de botones", ()=> {
+//   it('check visit to web page', () => {
+//       cy.visit('https://prueba-edward-vera.vercel.app/')
+//   })
+//   it("Deberia estar el boton +New presente", () => {
+//       cy.get('div.bg-gray-300').should('exist');
+//       // cy.get('button[name="buttonOpenModalCreateProduct"]').should('be.enabled')
+//       //cy.contains('button', '+ New');
+//   })
+// })
+
+/*
+bucar por clase
+cy.get(.buttonCreate)
+bucar por id
+cy.get(#buttonCreate)
+Si quiero ver deshabilitado el boton:
+.should('be.desabled')
+*/
--- client/src/components/dataTable/Header/Header.jsx
+++ client/src/components/dataTable/Header/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
   return (
     <div data-testid="headerTestId" className=' bg-gray-300 w-[100%] flex justify-between items-center border-t-2 border-b-2 border-gray-400 px-6 h-[60px]'>
         <h1 className=' text-blue-500 text-lg sm:text-2xl'><b>Manage Products</b></h1>
-        <button name="buttonOpenModalCreateProduct" className=' mx-1 px-3 w-[90px] sm:w-[100px] h-[40px] bg-green-500 rounded-lg text-base sm:text-lg text-white cursor-pointer' onClick={handleNewButtonClick}><b>+ New</b></button>
+        <button className=' mx-1 px-3 w-[90px] sm:w-[100px] h-[40px] bg-green-500 rounded-lg text-base sm:text-lg text-white cursor-pointer' onClick={handleNewButtonClick}><b>+ New</b></button>
         {isModalOpen && <Modal onClose={handleCloseModal} />}
     </div>
   )
